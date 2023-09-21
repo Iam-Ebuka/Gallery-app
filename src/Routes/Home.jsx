a3e9d9b429773646856a09c580737b85057766ad
Iam-Ebuka <ebukagrizz@gmail.com>
2023-09-21 12:18:57 +0100

    added the drag and drop effect

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,9 +2,58 @@ import { signOut, getAuth } from "firebase/auth"
 import './Home.css'
 import { useState } from "react";
 import { Picx } from "../Components/Picx";
+import { closestCenter, DndContext } from '@dnd-kit/core';
+import {
+    SortableContext,
+    verticalListSortingStrategy,
+    useSortable,
+    arrayMove
+} from '@dnd-kit/sortable'
+import {CSS} from '@dnd-kit/utilities';
+
+const SortablePicture = ({pic,index}) => {
+     const {
+        attributes, 
+        listeners, 
+        setNodeRef, 
+        transform, 
+        transition} = useSortable( {id: pic.id} );
+     const style = {
+            transition,
+            transform: CSS.Transform.toString(transform)
+        }
+    return (
+        <div 
+        ref={setNodeRef} 
+        style={style} 
+        {...attributes} 
+        {...listeners} 
+        className={
+        index === 0? "": 
+        index===1? "v-stretch":
+        index===2? 'h-stretch':
+        index===3? '':
+        index===4? '':
+        index===5? 'v-stretch':
+        index===6? 'big-stretch':
+        index===7? '': 
+        index===8? 'h-stretch':
+        index===9? '':
+        index===10? '':
+        index===11?'':
+        index===12?'v-stretch':
+        index===13? '':
+        index===14?'big-stretch': ''}>
+            <img src={pic.src} alt={pic.alt}/>
+        </div>
+    )
+}
 
 export function Home(){
     const [query, setQuery] = useState('');
+    const [pics, setPics] = useState(Picx)
+
+
     const auth = getAuth()
     async function handleSignOut(){
         try {
@@ -13,6 +62,18 @@ export function Home(){
             console.log(error)
         }
     }
+    const onDragEnd = event => {
+        const {active, over} = event
+        if (active.id === over.id) {
+            return;
+        }
+        setPics((pics) => {
+            const oldIndex = pics.findIndex((pic) => pic.id === active.id);
+            const newIndex = pics.findIndex((pic) => pic.id === over.id);
+            return arrayMove(pics, oldIndex, newIndex);
+        });
+    };
+
     return (
         <div>
             <header>
@@ -29,11 +90,15 @@ export function Home(){
                 
                     
                     <div className="grid-wrapper">
-                        {Picx.filter((pic) => 
+                        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+                            <SortableContext items={Picx} strategy={verticalListSortingStrategy}>
+                        {pics.filter((pic) => 
                         pic.name.toLowerCase().includes(query)
-                        ).map((pic) => (
-                            <div className={pic.class} key={pic.id}><img src={pic.src} alt={pic.alt}></img></div>
+                        ).map((pic, index) => (
+                            <SortablePicture key={pic.id} pic={pic} index={index}/>
                         ))}
+                        </SortableContext>
+                        </DndContext>
                     </div>
                 
             </main>
